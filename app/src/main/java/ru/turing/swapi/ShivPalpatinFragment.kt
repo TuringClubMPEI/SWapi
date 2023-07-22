a698042fa6a983b500a30cb2e26d9f8a24dee60e
konstantin <serebryakov.kostya@gmail.com>
2023-07-22 20:18:00 +0300

    Hw1

--- app/src/main/java/ru/turing/swapi/ShivPalpatinFragment.kt
+++ app/src/main/java/ru/turing/swapi/ShivPalpatinFragment.kt
@@ -5,55 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.turing.swapi.databinding.FragmentDartVaderBinding
+import ru.turing.swapi.databinding.FragmentLuckSkywalkerBinding
+import ru.turing.swapi.databinding.FragmentShivPalpatinBinding
+import ru.turing.swapi.databinding.FragmentMainScreenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ShivPalpatinFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ShivPalpatinFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentShivPalpatinBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shiv_palpatin, container, false)
+    ): View {
+        binding = FragmentShivPalpatinBinding.inflate(inflater, container, false)
+
+        binding.fromShivPalpatinFragmentToMainFragment.setOnClickListener {
+            if (activity is ActivityNavigator) {
+                (activity as ActivityNavigator).goToMainFragment()
+            }
+        }
+        return binding.root
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ShivPalpatinFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ShivPalpatinFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = ShivPalpatinFragment()
     }
 }
